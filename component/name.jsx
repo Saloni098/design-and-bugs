0ad17b4f89dac5ee16e5a958c8cbededa90c1ceb
Saloni Dwivedi <83643870+Saloni098@users.noreply.github.com>
2023-06-12 15:56:50 +0530

    Updated name.jsx

--- component/name.jsx
+++ component/name.jsx
@@ -9,8 +9,8 @@ export default function Name() {
             <div className = "column" >
             <div className = "box">
             <Icon icon = "noto:crown" color = "#2a2a3c" height = "100" /> 
-            <h2 className = "h1b"> Vaishnavi Dwivedi </h2> 
-            <p className = "boday"> full-stack product designer </p>
+            <h2 className = "h1b"> Saloni Dwivedi </h2> 
+            <p className = "boday"> designer and a developer </p>
             <div className = "box">
                         <div className = "cv-alt pointer">
                             <Link href="/about">
@@ -23,17 +23,16 @@ export default function Name() {
                     </div>
                     <br></br>
             <div className='cv2'>
-            <a target="_blank" href='https://github.com/dwvicy'><Icon  className="icony"icon="entypo-social:github-with-circle" color="#2a2a3c" height="40" /> </a>
-            <a target="_blank" href='https://linkedin.com/in/dwvicy'><Icon className="icony" icon="entypo-social:linkedin-with-circle" color="#2a2a3c" height="40" /></a>
-            <a target="_blank" href='https://youtube.com/c/dwvicy'><Icon  className="icony" icon="entypo-social:youtube-with-circle" color="#2a2a3c" height="40" /></a>
-            <a target="_blank" href='https://twitter.com/dwvicy'><Icon className="icony" icon="entypo-social:twitter-with-circle" color="#2a2a3c" height="40" /></a>
-            <a target="_blank" href='https://medium.com/@dwvicy'><Icon  className="icony" icon="entypo-social:medium-with-circle" color="#2a2a3c" height="40" /></a>
+            <a target="_blank" href='https://github.com/Saloni098'><Icon  className="icony"icon="entypo-social:github-with-circle" color="#2a2a3c" height="40" /> </a>
+            <a target="_blank" href='https://www.linkedin.com/in/saloni-dwivedi-b62262206/'><Icon className="icony" icon="entypo-social:linkedin-with-circle" color="#2a2a3c" height="40" /></a>
+            <a target="_blank" href='https://twitter.com/layzmaxx'><Icon className="icony" icon="entypo-social:twitter-with-circle" color="#2a2a3c" height="40" /></a>
+            <a target="_blank" href='https://medium.com/@saloni.dwivedi09'><Icon  className="icony" icon="entypo-social:medium-with-circle" color="#2a2a3c" height="40" /></a>
             </div>
             <div className='rowm'>
             <Icon icon="bytesize:code" color="#222" height="20" className='icoo'/> 
             <h1 className="footer">with</h1> 
             <Icon icon="noto:sparkling-heart" color="#222" height="20" className='icoo'/>
-            <h1 className='footer'>by dwvicy</h1>
+            <h1 className='footer'>by saloni</h1>
             </div>
             </div>
             </div>
