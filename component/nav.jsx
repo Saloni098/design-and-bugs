c67c366b77e8700e8398f460570bee4212c5651a
Saloni Dwivedi <83643870+Saloni098@users.noreply.github.com>
2023-06-12 15:59:09 +0530

    Updated nav.jsx

--- component/nav.jsx
+++ component/nav.jsx
@@ -22,10 +22,10 @@ export default function Nav() {
         <span className='nav-icon'><Link href='/projects'><a><Icon className="navy-icon "icon="noto:artist-palette" color="#2a2a3c" height="27"/></a></Link> </span>
         <span className='label'> <Link href='/projects'><a>code</a></Link> </span>
         </div>
-        <div className="div5n"> 
-        <span className='nav-icon'><Link href='/talks'><a><Icon className="navy-icon "icon="noto:studio-microphone" color="#2a2a3c" height="27"/></a></Link></span>
-        <span className='label'><a href='https://www.polywork.com/dwvicy/collections/1483' target="_blank">talks</a> </span>
-        </div>
+//         <div className="div5n"> 
+//         <span className='nav-icon'><Link href='/talks'><a><Icon className="navy-icon "icon="noto:studio-microphone" color="#2a2a3c" height="27"/></a></Link></span>
+//         <span className='label'><a href='https://www.polywork.com/dwvicy/collections/1483' target="_blank">talks</a> </span>
+//         </div>
         <div className="div6n"> 
         <span className='nav-icon'><Link href='/case-studies'><a><Icon className="navy-icon "icon="noto:bar-chart" color="#2a2a3c" height="27"/></a></Link></span>
         <span className='label'> <Link href='/case-studies'><a>design</a></Link> </span>
